bc1fbc8f9da612c11bb78f2ca6c970b219f625b1
Jay <35229@ma-web.nl>
2023-01-25 22:46:53 +0100

    update

--- Landing page/js/main.js
+++ Landing page/js/main.js
@@ -1,8 +1,9 @@
-const button = document.getElementById("js--menu");
+const button = document.getElementById("js--menu")
 const navigation = document.getElementById("js--nav");
+const body = document.getElementById("js--body")
 
-
-console.log(navigation)
 button.onclick = function() {
     navigation.style.visibility = "visible";
+    navigation.style.opacity ="1";
+    body.style.overflow = "hidden";
 }
